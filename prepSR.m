3676fced5a5a37ad29934f8f340bd77521ef7197
Ethan Blackwood <ethanbblackwood@gmail.com>
2020-02-28 15:33:18 -0500

    Remove startup and add paths to prepSR

--- prepSR.m
+++ prepSR.m
@@ -2,6 +2,8 @@ global synology_dir;
 global project_dir;
 global raw_dir;
 global processed_lfp_dir;
+global code_dir;
+global results_dir;
 
 if isempty(synology_dir)
     if ispc
@@ -14,3 +16,9 @@ end
 project_dir = fullfile(synology_dir, 'brenna', 'States_rats');
 raw_dir = fullfile(project_dir, 'RawData');
 processed_lfp_dir = fullfile(project_dir, 'meanSubtracted_fullTrace');
+code_dir = fileparts(mfilename('fullpath'));
+results_dir = fullfile(project_dir, 'forEthan');
+
+addpath(fullfile(synology_dir, 'Andi', 'Matlab'));
+addpath(fullfile(synology_dir, 'brenna', 'eeglab2019_1'));
+addpath(fullfile(code_dir, 'misc'));
