800b259344cbcade85a46649f11012a9069a1ff4
Ethan Blackwood <ethanbblackwood@gmail.com>
2020-08-29 21:25:22 -0400

    Expand organize_lfp to be able to select channels, and add get_channel_locs

---
+++ get_channel_locs.m
@@ -0,0 +1,85 @@
+function channel_locs = get_channel_locs(data_s, chan_s, models)
+% Get physical relative locations for selected channels on selected probes.
+%
+% Input data_s is a struct or matfile object obtained from loading a raw or
+% preprocessed data file.
+%
+% Input chan_s is a struct of selected channels. See organize_lfp for accepted formats
+% (the section regarding "when chans is a struct").
+%
+% If 'models' is provided, it should be a cell of the same length as the number of fields in chan_s,
+% with each entry a string specifying the corresponding probe model, 
+% or empty to to infer based on the probe name in the recording info (which is also
+% the default if 'models' is not given).
+%
+% Output is struct with the same fields as chan_s. The values are k x ndims matrices,
+% where k is the number of channels selected for the given probe and ndims
+% is the dimensionality of the probe's channel map. Values are the relative position
+% in each dimension in um, such that the distance in um between two channels i, j
+% can be obtained with:
+%
+%   norm(channel_locs(i,:) - channel_locs(j,:))
+
+ifo = data_s.info;
+
+probe_names = fieldnames(chan_s);
+n_probes = length(probe_names);
+
+for kP = 1:n_probes
+    name = probe_names{kP};
+    req_chans = chan_s.(name);
+
+    if exist('models', 'var') && ~isempty(models{kP})
+        model = models{kP};
+    else
+        % annoying thing
+        if startsWith(name, 'grid') || startsWith(name, 'ecog')
+            model = ifo.ecogGridName;
+        else
+            model = ifo.([name, 'Name']);
+        end
+    end
+
+    % get location map depending on probe model
+    if startsWith(model, 'H3')
+        % Cambridge Neurotech 64-channel
+        loc_map = num2cell((1:64)' * 20);
+
+    elseif startsWith(model, 'H4')
+        % Cambridge Neurotech 32-channel
+        loc_map = num2cell((1:32)' * 25);
+
+    elseif startsWith(model, 'E64-500-20-60')
+        % NeuroNexus ECoG array
+        [xloc, yloc] = meshgrid((1:6) * 500, (1:11) * 500);
+
+        % mark locations with no real electrode
+        xloc(11, 3:4) = nan;
+        yloc(11, 3:4) = nan;
+
+        % combine
+        loc_map = arrayfun(@(x, y) [y, x], xloc, yloc, 'uni', false);
+
+    else
+        error('Name %s does not match a known probe model', model);
+    end
+
+    % now get the entries corresponding to the selected channels
+    if islogical(req_chans)
+        req_locs = vertcat(loc_map{req_chans});
+
+    elseif isvector(req_chans)
+        loc_map_valid = loc_map(cellfun(@(loc) all(~isnan(loc)), loc_map));
+        req_locs = vertcat(loc_map_valid{req_chans});
+
+    else
+        % convert subscripts to linear indices first, then proceed as in logical indexing
+        subs = num2cell(req_chans, 1);
+        lin_inds = sub2ind(size(loc_map), subs{:});
+        req_locs = vertcat(loc_map{lin_inds});
+    end
+
+    channel_locs.(name) = req_locs;
+end
+
+end
--- organize_lfp.m
+++ organize_lfp.m
@@ -1,22 +1,146 @@
-function lfp_organized = organize_lfp(data_s)
-% returns a nchan x ntime LFP matrix with channels in data_s reordered according to
-% map in data_s.
+function lfp_organized = organize_lfp(data_s, chans)
+% Returns a nchan x ntime LFP matrix with channels in data_s reordered according to
+% map in data_s. The input can also be a matfile object.
 %
-% note that this switches probe 1 and probe 2 in order to put visual before
+% Note that this switches probe 1 and probe 2 in order to put visual before
 % motor, which is currently my convention.
+%
+% When 'chans' is not passed or empty, returns all channels in default order, which is Probe2 (V1)
+% before Probe1 (M1) or 'grid' before 'fork', and within each probe channels are in matrix order
+% according to the channel map which is superficial to deep, 'left' to 'right'.
+%
+% When chans is a double vector, this default order will be indexed by chans to produce the output
+% channels.
+%
+% When chans is a struct, each field is a probe name (e.g. 'Probe1', 'Probe2', 'grid', 'fork') and
+% the corresponding value is either:
+%   * a vector of linear indices into the given probe
+%   * a logical matrix the same shape as the channel map (e.g. 64x1 for H3 ('fork'), 32x1 for H4,
+%     and 11x6 for 'grid', where the center 2 entries in the last row are ignored because they
+%     do not correspond to real channels)
+%   * a k by ndim matrix of indices into the channel map (like the inputs to sub2ind), in order to
+%     get channels by subscript in a specific order. k is the number of channels requested, ndim
+%     is the number of dimensions of the channel map.
+% The output is the concatenated channels selected from each probe, in the order the fields are
+% listed in the struct.
+%
+% Output will be the channels extracted from each entry, concatenated in order.
 
 % depending on whether we have raw or mean-subtracted/artifact-removed
-% data, field containing data will be differed.
+% data, field containing data will be different.
 
-lfp_fields = {'meanSubFullTrace', 'meanSubData'}; % second one is for "snippits"
-if any(isfield(data_s, lfp_fields))
-    lfp_field = lfp_fields{isfield(data_s, lfp_fields)};
+if isstruct(data_s)
+    in_ds = @isfield;
 else
-    warning('Input appears to be raw data - may contain artifacts, noise, etc.');
-    lfp_field = 'LFPData';
-    assert(isfield(data_s, lfp_field), 'LFP data not found in loaded dataset.');
+    assert(isa(data_s, 'matlab.io.MatFile'), 'Invalid data_s');
+    in_ds = @isprop;
 end
 
-lfp_organized = data_s.(lfp_field)([data_s.info.Probe2Indicies, data_s.info.Probe1Indicies], :, :);
+if in_ds(data_s, 'meanSubFullTrace')
+    lfp_field = 'meanSubFullTrace';
+elseif in_ds(data_s, 'meanSubData') % for "snippits"
+    lfp_field = 'meanSubData';
+else
+    assert(in_ds(data_s, 'LFPData'), 'LFP data not found in loaded dataset.');
+    warning('Input appears to be raw data - may contain artifacts, noise, etc.');
+    lfp_field = 'LFPData';
+end
+
+% find what indices we have
+ifo = data_s.info;
+info_fields = fieldnames(ifo);
+probe_names = regexp(info_fields, '^.*(?=Indicies$)', 'match', 'once');
+probe_names = probe_names(~cellfun('isempty', probe_names));
+n_probes = length(probe_names);
+
+% make corresponding struct of absolute indices into lfp_field
+probe_indices = struct;
+probe_indices_withinvalid = struct;
+for kP = 1:n_probes
+    name = probe_names{kP};
+    indices = ifo.([name, 'Indicies']);
+    valid_inds = indices ~= 0;
+    lin_indices_valid = indices(valid_inds);  % remove non-existent channels
+
+    if ismember(1, lin_indices_valid)
+        % indices are possible relative to channels, rather than absolute
+        lin_indices_valid = ifo.([strrep(name, 'grid', 'ecog'), 'Channels'])(lin_indices_valid);
+        indices(valid_inds) = lin_indices_valid;
+    end
+
+    probe_indices.(name) = lin_indices_valid(:);
+    probe_indices_withinvalid.(name) = indices;
+end
+cat_indices = cell2mat(struct2cell(probe_indices));
+assert(length(unique(cat_indices)) == length(cat_indices), 'Probe channels overlap - something''s fishy');
+
+
+% if no 'chans' provided, default to all
+if ~exist('chans', 'var') || isempty(chans)
+    chans = 1:length(cat_indices);
+end
+
+if isstruct(chans)
+    req_probes = fieldnames(chans);
+    n_req = length(req_probes);
+
+    indices_to_use = cell(n_req, 1);
+    for kP = 1:n_req
+        probename = req_probes{kP};
+        req_chans = chans.(probename);
+
+        % normalize probe name a bit
+        if strncmp(probename, 'ecog', 4)
+            probename = 'grid';
+        end
+
+        % choose indices based on type of req_chans
+        if islogical(req_chans)
+            % index into precomputed indices map still in original shape (including invalid channels)
+            req_inds = probe_indices_withinvalid.(probename)(req_chans);
+            assert(all(req_inds > 0), 'Channel selection for %s included invalid channels', probename);
+
+            indices_to_use{kP} = req_inds(:);
+
+        elseif isvector(req_chans)
+            indices_to_use{kP} = probe_indices.(probename)(req_chans);
+        else
+            % convert subscripts to linear indices first, then proceed as in logical indexing
+            subs = num2cell(req_chans, 1);
+            lin_inds = sub2ind(size(probe_indices_withinvalid.(probename)), subs{:});
+
+            req_inds = probe_indices_withinvalid.(probename)(lin_inds);
+            assert(all(req_inds > 0), 'Channel selection for %s included invalid channels', probename);
+
+            indices_to_use{kP} = req_inds(:);
+        end
+    end
+    indices_to_use = cell2mat(indices_to_use);
+
+else
+    % this is all legacy crap that doesn't make much sense. use the struct form if possible.
+
+    % put them in the default order
+    probe2pos = find(strcmp(probe_names, 'Probe2'), 1); % for V1
+    if ~isempty(probe2pos)
+        probe_names = circshift(probe_names, 1 - probe2pos);
+    end
+
+    gridpos = find(strcmp(probe_names, 'grid'), 1);
+    if ~isempty(gridpos)
+        probe_names = circshift(probe_names, 1 - gridpos);
+    end
+
+    cat_indices = cell(n_probes, 1);
+    for kP = 1:n_probes
+        cat_indices{kP} = probe_indices.(probe_names{kP});
+    end
+    cat_indices = cell2mat(cat_indices);
+
+    indices_to_use = cat_indices(chans);
+end
+
+lfp_all = data_s.(lfp_field); % for mfile
+lfp_organized = lfp_all(indices_to_use, :, :);
 
 end
