cfad21a546c8a438e6cff2e19536331c8447941b
Ethan Blackwood <ethanbblackwood@gmail.com>
2020-10-06 02:19:01 -0400

    Pull rescale_to_pmf out of kl_divergence_analysis

--- pipeline/kl_divergence_analysis.m
+++ pipeline/kl_divergence_analysis.m
@@ -134,24 +134,6 @@ end
 
 end
 
-function [new_U, varargout] = rescale_to_pmf(U, varargin)
-% Makes V or cell of Vs (from NMF) into a series of probability mass functions by the following
-% steps:
-%  * Divide each V by the maximum sum over components over all Vs (such that they each sum to <= 1
-%    at each point), and multiply U by the same factor.
-%  * Add a column to U consisting of all zeros
-%  * Add a column to each V consisting of 1 - sum(V, 2) (such that all rows sum to 1).
-
-scale_factor = max(cellfun(@(v) max(sum(v, 2)), varargin));
-scale_factor = scale_factor + eps(scale_factor); % make sure there's no discrepancy when dividing each element
-Vs = cellfun(@(v) v / scale_factor, varargin, 'uni', false);
-U = U * scale_factor;
-
-new_U = [U, zeros(size(U, 1), 1)];
-varargout = cellfun(@(v) [v, 1 - sum(v, 2)], Vs, 'uni', false);
-
-end
-
 function X = optimize_xentropy(Vi, Vj)
 % Get the matrix X that minimizes cross-entropy of Vi*X relative to Vj.
 
