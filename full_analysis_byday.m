f6585d4d15749ad18ac8111aa99075ce939f84fe
Ethan Blackwood <ethanbblackwood@gmail.com>
2021-03-19 11:04:22 -0400

    Update analysis script, make matrix plotting fn more general

--- full_analysis_byday.m
+++ full_analysis_byday.m
@@ -4,44 +4,71 @@
 
 sr_dirs = prepSR;
 
-days = {
-%     '2020-01-30'
-%     '2020-01-31'
-%     '2020-02-06'
-%     '2020-03-05'
-%     '2020-03-06'
-%     '2020-03-10'
-%     '2020-03-11'
-    '2020-10-26'
-    '2020-10-27'
-    '2020-10-28'
-    '2020-10-29'
-    };
+exp_info = [struct('type', 'm1_v1', 'days', {{
+%         '2020-01-30'
+%         '2020-01-31'
+%         '2020-02-06'
+%         '2020-03-05'
+%         '2020-03-06'
+%         '2020-03-10'
+%         '2020-03-11'
+        % not using any of the above - short probe recordings
+        '2020-10-26'
+        '2020-10-27'
+        '2020-10-28'
+        '2020-10-29'
+    }})
+    struct('type', 'bilateral', 'days', {{
+        '2021-01-27'
+        '2021-01-29'
+        '2021-01-31'
+        '2021-02-02'
+    }})];
 
-n_days = length(days);
+% set up full channel name lists for analyses later
+layer_names = [
+    arrayfun(@(k) ['Sup', num2str(k)], 8:-1:1, 'uni', false), {'L4'}, ...
+    arrayfun(@(k) ['Inf', num2str(k)], 1:8, 'uni', false)
+    ];
+
+exp_info(1).all_chan_names = [strcat('M1_', layer_names), strcat('V1_', layer_names)];
+exp_info(2).all_chan_names = [strcat('V1L_', layer_names), strcat('V1R_', layer_names)];
+
+exp_types = {exp_info.type};
+all_days = vertcat(exp_info.days);
+
+n_days = length(all_days);
 
 %% Collect datasets on each day
-input_s = struct('name', days, ...
-    'mt_res_in', cell(n_days, 1), ...
-    'nmf_res_out', cell(n_days, 1), ...
-    'xval_fig_dir', cell(n_days, 1));
 
-for kD = 1:n_days
-    % Build input to concat_and_nmf
-    
-    curr_day = days{kD};
-    input_s(kD).nmf_res_out = fullfile(sr_dirs.results, curr_day, 'nmf_res.mat');
-    input_s(kD).xval_fig_dir = fullfile(sr_dirs.results, curr_day);
-    
-    % find all "layers" results files under this day
-    res_fn = 'mt_res_layers.mat';
-    res_entries = dir(fullfile(sr_dirs.results, curr_day, '*', res_fn));
-    dirs = sort({res_entries.folder});
-    input_s(kD).mt_res_in = fullfile(dirs, res_fn);
+for kE = 1:length(exp_types)
+    this_days = exp_info(kE).days;
+    this_ndays = length(this_days);
+    exp_info(kE).input_s =  struct(...
+        'name', this_days, ...
+        'mt_res_in', cell(this_ndays, 1), ...
+        'nmf_res_out', cell(this_ndays, 1), ...
+        'xval_fig_dir', cell(this_ndays, 1));
+
+    for kD = 1:this_ndays
+        % Build input to concat_and_nmf
+
+        curr_day = this_days{kD};
+        exp_info(kE).input_s(kD).nmf_res_out = fullfile(sr_dirs.results, curr_day, 'nmf_res.mat');
+        exp_info(kE).input_s(kD).xval_fig_dir = fullfile(sr_dirs.results, curr_day);
+
+        % find all "layers" results files under this day
+        res_fn = 'mt_res_layers.mat';
+        res_entries = dir(fullfile(sr_dirs.results, curr_day, '*', res_fn));
+        dirs = sort({res_entries.folder});
+        exp_info(kE).input_s(kD).mt_res_in = fullfile(dirs, res_fn);
+    end
 end
 
+input_s_all = vertcat(exp_info.input_s);
+
 %% Do NMF
-nmf_mfiles = concat_and_nmf(input_s);
+nmf_mfiles = concat_and_nmf(input_s_all);
 
 %% Make null model data
 gen_null_model_data(nmf_mfiles);
@@ -54,146 +81,154 @@ score_dist_analysis(nmf_mfiles, 'L2_dist');
 
 %% Mutual information analysis - now normalized
 
-for kD = 1:n_days
-    mfile = nmf_mfiles{kD};
-    classes_cell = mfile.nmf_classes;
-    % just use run 1 of 2 (arbitrarily)
-    classes_cell = classes_cell{1};
-    chans = mfile.kl_chans;
+for kE = 1:length(exp_info)
+    this_info = exp_info(kE);
+    this_ndays = length(this_info.days);
+    chan_names = this_info.all_chan_names;
+    n_chans = length(chan_names);
+    mut_info_combined = nan(n_chans, n_chans, this_ndays);
+    
+    for kD = 1:this_ndays
+        this_day = this_info.input_s(kD).name;
+        mfile = matfile(this_info.input_s(kD).nmf_res_out);
+        
+        classes_cell = mfile.nmf_classes;
+        % just use run 1 of 2 (arbitrarily)
+        classes_cell = classes_cell{1};
+        chans = mfile.all_chans;
 
-    classes = horzcat(classes_cell{:});
-    [~, norm_mut_info] = class_mut_info(classes);
-    fh2 = figure;
-    sanePColor(norm_mut_info);
-    set(gca, 'YDir', 'reverse');
-    set(gca, 'TickLabelInterpreter', 'none');
-    xticks(1:length(chans));
-    xticklabels(chans);
-    xtickangle(45);
-    yticks(1:length(chans));
-    yticklabels(chans);
-    c = colorbar;
-    colormap('jet');
-    title(sprintf('Norm. mut. info of classes between electrodes (%s)', days{kD}));
+        classes = horzcat(classes_cell{:});
+        [~, norm_mut_info] = class_mut_info(classes);
 
-    savefig(fh2, fullfile(sr_dirs.results, days{kD}, sprintf('norm_mut_info_%s.fig', days{kD})));
+        fh = plot_dist_mat(norm_mut_info, chans, this_day, 'norm_mutual_info');
+        savefig(fh, fullfile(sr_dirs.results, this_day, sprintf('norm_mut_info_%s.fig', this_day)));
+        
+        % save to combined array
+        insert_inds = cellfun(@(c) find(strcmp(chan_names, c)), chans);
+        mut_info_combined(insert_inds, insert_inds, kD) = norm_mut_info;
+    end
+    
+    % Take median and make combined plot
+    med_mut_info = nanmedian(mut_info_combined, 3);
+    
+    % eliminate channels with no data
+    chans_empty = all(isnan(med_mut_info)) & all(isnan(med_mut_info'));
+    chan_names = chan_names(~chans_empty);
+    med_mut_info = med_mut_info(~chans_empty, ~chans_empty);
+    
+    fh = plot_dist_mat(med_mut_info, chan_names, ...
+        sprintf('%s, median of %d days', this_info.type, this_ndays), 'norm_mutual_info');
+    savefig(fh, fullfile(sr_dirs.results, 'res_figs', sprintf('norm_mut_info_%s.fig', this_info.type)));
 end
 
 %% Transition synchrony analysis
-% Get transitions for each day
-trans_tables = cell(n_days, 1);
-for kD = 1:n_days
-    mt_paths = input_s(kD).mt_res_in;
-    nmf_path = input_s(kD).nmf_res_out;
-    trans_tables{kD} = get_state_transitions(mt_paths, nmf_path);
+for kE = 1:length(exp_info)
+    this_info = exp_info(kE);
+    this_ndays = length(this_info.days);
+    trans_tables = cell(this_ndays, 1);
     
-    % add SPIKE-Sync score
-    trans_tables{kD} = calc_transition_synchronization(trans_tables{kD});
-    
-    figure;
-    plot_transitions(trans_tables{kD});
-    title(sprintf('Transitions with sync scores - %s', input_s(kD).name));
-end
+    for kD = 1:this_ndays
+        mt_paths = this_info.input_s(kD).mt_res_in;
+        nmf_path = this_info.input_s(kD).nmf_res_out;
+        trans_tables{kD} = get_state_transitions(mt_paths, nmf_path);
 
-% Make CDF of sync scores and compare to bootstrap
-n_bootstrap = 1000;
+        % add SPIKE-Sync score
+        trans_tables{kD} = calc_transition_synchronization(trans_tables{kD});
 
-figure;
-sync_scores_all = cellfun(@(tt) tt.sync_score, trans_tables, 'uni', false);
-sync_scores_all = vertcat(sync_scores_all{:});
-[real_cdf, bin_edges] = histcounts(sync_scores_all, 'Normalization', 'cdf');
-bin_centers = mean([bin_edges(1:end-1); bin_edges(2:end)]);
-plot(bin_centers, real_cdf, '.-');
-hold on;
-
-boot_cdf = zeros(n_bootstrap, length(bin_centers));
-for kB = 1:n_bootstrap
-    shuffled_sync_scores = cell(n_days, 1);
-    
-    for kD = 1:n_days
-        trans_shuffled = shuffle_trans_dwell_times(trans_tables{kD});
-        trans_shuffled = calc_transition_synchronization(trans_shuffled);
-        shuffled_sync_scores{kD} = trans_shuffled.sync_score;
+        fh = figure;
+        plot_transitions(trans_tables{kD});
+        title(sprintf('Transitions with sync scores - %s', this_info.input_s(kD).name));
+        savefig(fh, fullfile(sr_dirs.results, this_info.days{kD}, ...
+            sprintf('transitions_w_sync_%s.fig', this_info.days{kD})));
     end
-    shuffled_sync_scores = vertcat(shuffled_sync_scores{:});
-    boot_cdf(kB, :) = histcounts(shuffled_sync_scores, bin_edges, 'Normalization', 'cdf');
+
+    % Make CDF of sync scores and compare to bootstrap
+    n_bootstrap = 1000;
+
+    fh = figure;
+    sync_scores_all = cellfun(@(tt) tt.sync_score, trans_tables, 'uni', false);
+    sync_scores_all = vertcat(sync_scores_all{:});
+    [real_cdf, bin_edges] = histcounts(sync_scores_all, 'Normalization', 'cdf');
+    bin_centers = mean([bin_edges(1:end-1); bin_edges(2:end)]);
+    plot(bin_centers, real_cdf, '.-');
+    hold on;
+
+    boot_cdf = zeros(n_bootstrap, length(bin_centers));
+    for kB = 1:n_bootstrap
+        shuffled_sync_scores = cell(this_ndays, 1);
+
+        for kD = 1:this_ndays
+            trans_shuffled = shuffle_trans_dwell_times(trans_tables{kD});
+            trans_shuffled = calc_transition_synchronization(trans_shuffled);
+            shuffled_sync_scores{kD} = trans_shuffled.sync_score;
+        end
+        shuffled_sync_scores = vertcat(shuffled_sync_scores{:});
+        boot_cdf(kB, :) = histcounts(shuffled_sync_scores, bin_edges, 'Normalization', 'cdf');
+    end
+
+    % Plot median and 95% CI of bootstrap CDFs with error bars
+    boot_cdf_quantiles = quantile(boot_cdf, [0.025, 0.5, 0.975]);
+    neg = boot_cdf_quantiles(2,:) - boot_cdf_quantiles(1, :);
+    pos = boot_cdf_quantiles(3,:) - boot_cdf_quantiles(2,:);
+    errorbar(bin_centers, boot_cdf_quantiles(2,:), neg, pos, '.-');
+    title(sprintf('Synchronization scores for all days (%s) (%d bootstraps)', ...
+        this_info.type, n_bootstrap), 'Interpreter', 'none');
+    xlabel('Sync score');
+    ylabel('CDF');
+    legend('Real', 'Shuffled', 'Location', 'northwest');
+    savefig(fh, fullfile(sr_dirs.results, 'res_figs', ...
+        sprintf('transition_sync_cdf_boot_%s.fig', this_info.type)));
 end
 
-% Plot median and 95% CI of bootstrap CDFs with error bars
-boot_cdf_quantiles = quantile(boot_cdf, [0.025, 0.5, 0.975]);
-neg = boot_cdf_quantiles(2,:) - boot_cdf_quantiles(1, :);
-pos = boot_cdf_quantiles(3,:) - boot_cdf_quantiles(2,:);
-errorbar(bin_centers, boot_cdf_quantiles(2,:), neg, pos, '.-');
-title(sprintf('Synchronization scores for all days (%d bootstraps)', n_bootstrap));
-xlabel('Sync score');
-ylabel('CDF');
-legend('Real', 'Shuffled', 'Location', 'northwest');
+%% Get and plot median score distance for each channel pair
+
+for kE = 1:length(exp_info)
+    this_info = exp_info(kE);
+    this_ndays = length(this_info.days);
+    nmf_res_paths = {this_info.input_s.nmf_res_out}';
+    nmf_res_mfiles = cellfun(@matfile, nmf_res_paths, 'uni', false);
+
+    chan_names = this_info.all_chan_names;
+    n_chans = length(chan_names);
+
+    % gather distance information from each recording
+    all_dists = nan(n_chans, n_chans, this_ndays);
+    all_dists_null = nan(n_chans, n_chans, this_ndays);
+
+    for kD = 1:this_ndays
+        insert_inds = cellfun(@(c) find(strcmp(chan_names, c)), nmf_res_mfiles{kD}.chan_names);
+
+        this_mfile = nmf_res_mfiles{kD};
+        all_dists(insert_inds, insert_inds, kD) = this_mfile.allL2_dists;
+        all_dists_null(insert_inds, insert_inds, kD) = this_mfile.allL2_dists_null;
+    end
+
+    med_dists = nanmedian(all_dists, 3);
+
+    % eliminate channels with no data
+    chans_empty = all(isnan(med_dists)) & all(isnan(med_dists'));
+    chan_names = chan_names(~chans_empty);
+    med_dists = med_dists(~chans_empty, ~chans_empty);
+    all_dists = all_dists(~chans_empty, ~chans_empty, :);
+    all_dists_null = all_dists_null(~chans_empty, ~chans_empty, :);
+
+    hf = plot_dist_mat(med_dists, chan_names, ...
+        sprintf('%s - median over %d days', replace(this_info.type, '_', '\_'), this_ndays), 'L2_dist');
+    savefig(hf, fullfile(sr_dirs.results, 'res_figs', sprintf('med_kl_div_%s.fig', this_info.type)));
+    
+    % Same thing but with difference from null model
+    med_dists_from_null = nanmedian(all_dists_null - all_dists, 3);
+    hf = plot_dist_mat(med_dists_from_null, chan_names, ...
+        sprintf('%s (median)', replace(this_info.type, '_', '\_')), 'L2_dist_from_null');
+    savefig(hf, fullfile(sr_dirs.results, 'res_figs', sprintf('med_kl_div_fromnull_%s.fig', this_info.type)));
+end
 
 %% **** Below here is old - to be updated ****
 
-%% Get and plot median KL divergence for each channel pair (here still assuming V1 super...etc. labels are comparable)
-% For each day, use only chan names specified in the csd results file (since I went back and
-% filtered out channels that don't actually correspond to the layer they're supposed to be)
-
-%layers = {'L2/3', 'L4', 'L5', 'L5B'};
-layers = [
-    arrayfun(@(k) ['Sup', num2str(k)], 8:-1:1, 'uni', false), {'L4'}, ...
-    arrayfun(@(k) ['Inf', num2str(k)], 1:8, 'uni', false)
-    ];
-
-chans = [strcat('V1_', layers), strcat('M1_', layers)]; % how they will be plotted
-n_chans = length(chans);
-
-all_mean_kl_divs = nan(n_chans, n_chans, n_days);
-all_mean_kl_divs_null = nan(n_chans, n_chans, n_days);
-
-for kD = 1:n_days
-    res_kld = load(fullfile(sr_dirs.results, days{kD}, 'nmf_res.mat'), 'kl_divs', 'kl_divs_null', 'chan_names');
-    csd_chans_V1_s = load(fullfile(sr_dirs.results, days{kD}, 'csd_V1.mat'), 'chan_names');
-    try
-        csd_chans_M1_s = load(fullfile(sr_dirs.results, days{kD}, 'csd_M1.mat'), 'chan_names');
-    catch ME
-        if strcmp(ME.identifier, 'MATLAB:load:couldNotReadFile')
-            % probably saved as MC instead
-            csd_chans_M1_s = load(fullfile(sr_dirs.results, days{kD}, 'csd_MC.mat'), 'chan_names');
-        else
-            rethrow(ME);
-        end
-    end
-    
-    data_cnames = res_kld.chan_names;
-    
-    good_chan_set = [strcat('V1_', csd_chans_V1_s.chan_names), strcat('M1_', csd_chans_M1_s.chan_names)];
-    b_take = ismember(data_cnames, good_chan_set);
-    data_good_cnames = data_cnames(b_take);
-    insert_inds = cellfun(@(c) find(strcmp(chans, c)), data_good_cnames);
-    
-    all_mean_kl_divs(insert_inds, insert_inds, kD) = res_kld.kl_divs(b_take, b_take);
-    all_mean_kl_divs_null(insert_inds, insert_inds, kD) = res_kld.kl_divs_null(b_take, b_take);
-end
-
-med_kl_divs = nanmedian(all_mean_kl_divs, 3);
-
-% eliminate channels with no data
-chans_empty = all(isnan(med_kl_divs)) & all(isnan(med_kl_divs'));
-chans = chans(~chans_empty);
-med_kl_divs = med_kl_divs(~chans_empty, ~chans_empty);
-all_mean_kl_divs = all_mean_kl_divs(~chans_empty, ~chans_empty, :);
-all_mean_kl_divs_null = all_mean_kl_divs_null(~chans_empty, ~chans_empty, :);
-
-hf = plot_dist_mat(med_kl_divs, chans, sprintf('Median over %d days', n_days));
-savefig(hf, fullfile(sr_dirs.results, 'res_figs', 'med_kl_div_days.fig'));
-
-%% Try same thing but with difference from null model
-
-med_kl_div_from_null = nanmedian(all_mean_kl_divs_null - all_mean_kl_divs, 3);
-hf = plot_dist_mat(med_kl_div_from_null, chans, 'Amount below divergence from null model (median)');
-savefig(hf, fullfile(sr_dirs.results, 'res_figs', 'med_kl_div_fromnull_days.fig'));
-
-% make a graph plot out of it
-med_kl_div_from_null_nonnan = med_kl_div_from_null;
-med_kl_div_from_null_nonnan(isnan(med_kl_div_from_null)) = 0;
-g = digraph(med_kl_div_from_null, 'omitselfloops');
+%% make a graph plot out of it
+med_kl_div_from_null_nonnan = med_dists_from_null;
+med_kl_div_from_null_nonnan(isnan(med_dists_from_null)) = 0;
+g = digraph(med_dists_from_null, 'omitselfloops');
 ecolors = 5 * g.Edges.Weight / max(g.Edges.Weight);
 edge_labels = arrayfun(@(w) sprintf('%.2f', w), g.Edges.Weight, 'uni', false);
 
@@ -223,10 +258,10 @@ kl_divs_null_bytype = struct;
 for kT = 1:n_types
     %mean_kldiv_oftype = @(mat) mean(mat(linear_inds{kT}));
     
-    kl_divs_perday = cellfun(@(mat) mat(linear_inds{kT}), num2cell(all_mean_kl_divs, [1, 2]), 'uni', false);
+    kl_divs_perday = cellfun(@(mat) mat(linear_inds{kT}), num2cell(all_dists, [1, 2]), 'uni', false);
     kl_divs_bytype.(type_snames{kT}) = vertcat(kl_divs_perday{:});
     
-    kl_divs_null_perday = cellfun(@(mat) mat(linear_inds{kT}), num2cell(all_mean_kl_divs_null, [1, 2]), 'uni', false);
+    kl_divs_null_perday = cellfun(@(mat) mat(linear_inds{kT}), num2cell(all_dists_null, [1, 2]), 'uni', false);
     kl_divs_null_bytype.(type_snames{kT}) = vertcat(kl_divs_null_perday{:});
 end
 
--- pipeline/plot_dist_mat.m
+++ pipeline/plot_dist_mat.m
@@ -1,10 +1,11 @@
 function hf = plot_dist_mat(dist_mats, chans, title_line2, dist_type)
-% Plot a grid of KL divergences between pairs of channels (as obtained by kl_divergence_analysis)
+% Plot a grid of score distances between pairs of channels (as obtained by score_dist_analysis)
 %
 % Inputs:
 %   kl_div: n_chans x n_chans matrix of NMF score KL divergences
 %   chans: cell array of channel names
 %   title_line2: If nonempty, print this below the general figure title.
+%   dist_type: what kind of distance this is - see switch statement below for options
 
 mean_dist_mat = mean(dist_mats, 3);
 hf = figure;
@@ -15,23 +16,40 @@ if nargin < 4 || isempty(dist_type)
     dist_type = 'kl_div';
 end
 
+interpreter = 'latex';
+cmap = 'parula';
 switch dist_type
     case 'kl_div'
         title_line1 = 'NMF score KL divergence - $$\min_X \mathbf{E}[D_{KL}(V_j || V_iX)]$$';
     case 'L2_dist'
-        title_line1 = 'Norm of score difference - $$\min_X \|V_j - V_iX\|_F$$';
+        title_line1 = 'Euclidean score distance - $$\min_X \|V_j - V_iX\|_F$$';
+    case 'L2_dist_from_null'
+        title_line1 = 'Euclidean score proximity compared to null model';
     case 'recon_err'
         title_line1 = 'Spectrogram reconstruction error of channel j from channel i';
+        interpreter = 'none';
     case 'recon_from_kl_div'
         title_line1 = 'Reconstruction error from minimizing score KL divergence';
+        interpreter = 'none';
     case 'recon_from_L2_dist'
         title_line1 = 'Reconstruction error from minimizing norm of score difference';
+        interpreter = 'none';
+    case 'mutual_info'
+        title_line1 = 'Mutual information of classes between channels';
+        interpreter = 'none';
+        cmap = 'jet';
+    case 'norm_mutual_info'
+        title_line1 = 'Norm. mut. info. of classes between channels';
+        interpreter = 'none';
+        cmap = 'jet';
+    otherwise
+        error('Unrecognized matrix type');
 end
 
 if nargin < 3 || isempty(title_line2)
-    title(title_line1, 'Interpreter', 'latex');
+    title(title_line1, 'Interpreter', interpreter);
 else
-    title({title_line1, title_line2}, 'Interpreter', 'latex');
+    title({title_line1, title_line2}, 'Interpreter', interpreter);
 end
 
 xticks(1:length(chans));
@@ -45,6 +63,7 @@ set(gca, 'TickLabelInterpreter', 'none');
 ylabel('Channel i');
 xlabel('Channel j');
 
+colormap(cmap);
 colorbar;
 
 end
