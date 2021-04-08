3f1a52900ef0a629a708c1ed500fead540140824
Ethan Blackwood <ethanbblackwood@gmail.com>
2021-04-08 17:17:16 -0400

    Tweak full_analysis script

--- full_analysis_byday.m
+++ full_analysis_byday.m
@@ -94,48 +94,6 @@ gen_null_model_data(nmf_mfiles);
 %% Euclidean distance analysis of aligned scores
 score_dist_analysis(nmf_mfiles, 'L2_dist');
 
-%% Mutual information analysis - now normalized
-
-for kE = 1:length(exp_info)
-    this_info = exp_info(kE);
-    this_ndays = length(this_info.days);
-    chan_names = this_info.all_chan_names;
-    n_chans = length(chan_names);
-    mut_info_combined = nan(n_chans, n_chans, this_ndays);
-    
-    for kD = 1:this_ndays
-        this_day = this_info.input_s(kD).name;
-        mfile = matfile(this_info.input_s(kD).nmf_res_out);
-        
-        classes_cell = mfile.nmf_classes;
-        % just use run 1 of 2 (arbitrarily)
-        classes_cell = classes_cell{1};
-        chans = mfile.all_chans;
-
-        classes = horzcat(classes_cell{:});
-        [~, norm_mut_info] = class_mut_info(classes);
-
-        fh = plot_dist_mat(norm_mut_info, chans, this_day, 'norm_mutual_info');
-        savefig(fh, fullfile(sr_dirs.results, this_day, sprintf('norm_mut_info_%s.fig', this_day)));
-        
-        % save to combined array
-        insert_inds = cellfun(@(c) find(strcmp(chan_names, c)), chans);
-        mut_info_combined(insert_inds, insert_inds, kD) = norm_mut_info;
-    end
-    
-    % Take median and make combined plot
-    med_mut_info = median(mut_info_combined, 3, 'omitnan');
-    
-    % eliminate channels with no data
-    chans_empty = all(isnan(med_mut_info)) & all(isnan(med_mut_info'));
-    chan_names = chan_names(~chans_empty);
-    med_mut_info = med_mut_info(~chans_empty, ~chans_empty);
-    
-    fh = plot_dist_mat(med_mut_info, chan_names, ...
-        sprintf('%s, median of %d days', this_info.type, this_ndays), 'norm_mutual_info');
-    savefig(fh, fullfile(sr_dirs.results, 'res_figs', sprintf('norm_mut_info_%s.fig', this_info.type)));
-end
-
 %% Transition synchrony analysis
 for kE = 1:length(exp_info)
     this_info = exp_info(kE);
@@ -195,11 +153,14 @@ for kE = 1:length(exp_info)
         sprintf('transition_sync_cdf_boot_%s.fig', this_info.type)));
 end
 
-%% Get and plot median score distance for each channel pair
+%% Construct matrices of mutual information and score distance
 
-chan_names = cell(length(exp_info), 1);
+chan_names = cell(length(exp_info), 1); 
+hr_chan_names = cell(length(exp_info), 1); % human-readable
+mut_info_combined = cell(length(exp_info), 1);
 all_dists = cell(length(exp_info), 1);
 all_dists_null = cell(length(exp_info), 1);
+all_recon_err = cell(length(exp_info), 1);
 
 for kE = 1:length(exp_info)
     this_info = exp_info(kE);
@@ -208,42 +169,193 @@ for kE = 1:length(exp_info)
     nmf_res_mfiles = cellfun(@matfile, nmf_res_paths, 'uni', false);
 
     chan_names{kE} = this_info.all_chan_names;
+    hr_chan_names{kE} = util.make_hr_chan_names(chan_names{kE}, 140);
     n_chans = length(chan_names{kE});
 
     % gather distance information from each recording
+    mut_info_combined{kE} = nan(n_chans, n_chans, this_ndays);
     all_dists{kE} = nan(n_chans, n_chans, this_ndays);
     all_dists_null{kE} = nan(n_chans, n_chans, this_ndays);
-
+    all_recon_err{kE} = nan(n_chans, n_chans, this_ndays);
+    
     for kD = 1:this_ndays
-        insert_inds = cellfun(@(c) find(strcmp(chan_names{kE}, c)), nmf_res_mfiles{kD}.chan_names);
-
+        this_day = this_info.input_s(kD).name;
         this_mfile = nmf_res_mfiles{kD};
+        this_chans = this_mfile.all_chans;
+                
+        % compute mutual information
+        classes_cell = this_mfile.nmf_classes;
+        % just use run 1 of 2 (arbitrarily)
+        classes_cell = classes_cell{1};
+        classes = horzcat(classes_cell{:});
+        [~, norm_mut_info] = class_mut_info(classes);
+        
+        % plot mutual information
+        this_hr_chan_names = util.make_hr_chan_names(this_chans, 140);
+        fh = plot_dist_mat(norm_mut_info, this_hr_chan_names, this_day, 'norm_mutual_info');
+        savefig(fh, fullfile(sr_dirs.results, this_day, sprintf('norm_mut_info_%s.fig', this_day)));
+        
+        % save data to 3D array
+        insert_inds = cellfun(@(c) find(strcmp(chan_names{kE}, c)), this_chans);
+        mut_info_combined{kE}(insert_inds, insert_inds, kD) = norm_mut_info;
         all_dists{kE}(insert_inds, insert_inds, kD) = this_mfile.allL2_dists;
         all_dists_null{kE}(insert_inds, insert_inds, kD) = this_mfile.allL2_dists_null;
+        all_recon_err{kE}(insert_inds, insert_inds, kD) = this_mfile.allL2_dist_recon_err;
     end
+    
+    % find channels with no data
+    nonempty_chan_combos = any(~isnan(mut_info_combined{kE}), 3);
+    nonempty_chans = any(nonempty_chan_combos);
+    
+    chan_names{kE} = chan_names{kE}(nonempty_chans);
+    hr_chan_names{kE} = hr_chan_names{kE}(nonempty_chans);
+    mut_info_combined{kE} = mut_info_combined{kE}(nonempty_chans, nonempty_chans, :);
+    all_dists{kE} = all_dists{kE}(nonempty_chans, nonempty_chans, :);
+    all_dists_null{kE} = all_dists_null{kE}(nonempty_chans, nonempty_chans, :);
+    all_recon_err{kE} = all_recon_err{kE}(nonempty_chans, nonempty_chans, :);
+end
+
+%% Mutual information combined plots
+
+for kE = 1:length(exp_info)
+    this_info = exp_info(kE);
+    this_ndays = length(this_info.days);
+    
+    med_mut_info = median(mut_info_combined{kE}, 3, 'omitnan');
+
+    % matrix plot
+    fh = plot_dist_mat(med_mut_info, hr_chan_names{kE}, ...
+        sprintf('%s, median of %d days', this_info.type, this_ndays), 'norm_mutual_info');
+    savefig(fh, fullfile(sr_dirs.results, 'res_figs', sprintf('norm_mut_info_%s.fig', this_info.type)));
+    
+    % violin plot
+    [fh, pval] = make_violin_from_mats(mut_info_combined{kE}, chan_names{kE}, 'left', false);
+    ylabel('Normalized MI');
+    title({'Normalized mutual information of channel pairs', this_info.type}, 'Interpreter', 'none');
+    savefig(fh, fullfile(sr_dirs.results, 'res_figs', ...
+        sprintf('norm_mut_info_violin_%s.fig', this_info.type)));
+    
+    % stats    
+    exp_info(kE).nmf_pval = pval;
+    fprintf('NMI, %s, cross- vs. same-region:\n p = %g\n', this_info.type, pval);
+end
+
+%% Score distance combined plots
+
+for kE = 1:length(exp_info)
+    this_info = exp_info(kE);
+    this_ndays = length(this_info.days);
 
     med_dists = median(all_dists{kE}, 3, 'omitnan');
-
-    % eliminate channels with no data
-    chans_empty = all(isnan(med_dists)) & all(isnan(med_dists'));
-    chan_names{kE} = chan_names{kE}(~chans_empty);
-    med_dists = med_dists(~chans_empty, ~chans_empty);
-    all_dists{kE} = all_dists{kE}(~chans_empty, ~chans_empty, :);
-    all_dists_null{kE} = all_dists_null{kE}(~chans_empty, ~chans_empty, :);
-
-    % make human-readable channel names
-    hr_chan_names = util.make_hr_chan_names(chan_names{kE}, 140);
-
-    hf = plot_dist_mat(med_dists, hr_chan_names, ...
-        sprintf('%s - median over %d days', this_info.type, this_ndays), 'L2_dist');
-    savefig(hf, fullfile(sr_dirs.results, 'res_figs', sprintf('med_score_dist_%s.fig', this_info.type)));
-    
-    % Same thing but with difference from null model
     med_dists_from_null = median(all_dists_null{kE} - all_dists{kE}, 3, 'omitnan');
-    hf = plot_dist_mat(med_dists_from_null, hr_chan_names, ...
+    med_recon_err = median(all_recon_err{kE}, 3, 'omitnan');
+
+    % matrix plots
+    % Score distance
+    fh = plot_dist_mat(med_dists, hr_chan_names{kE}, ...
+        sprintf('%s - median over %d days', this_info.type, this_ndays), 'L2_dist');
+    savefig(fh, fullfile(sr_dirs.results, 'res_figs', sprintf('med_score_dist_%s.fig', this_info.type)));
+    
+    % Score dist, difference from null model
+    fh = plot_dist_mat(med_dists_from_null, hr_chan_names{kE}, ...
         sprintf('%s (median)', this_info.type), 'L2_dist_from_null');
-    savefig(hf, fullfile(sr_dirs.results, 'res_figs', sprintf('med_score_dist_fromnull_%s.fig', this_info.type)));
+    savefig(fh, fullfile(sr_dirs.results, 'res_figs', sprintf('med_score_dist_fromnull_%s.fig', this_info.type)));
+    
+    % Reconstruction error
+    fh = plot_dist_mat(med_recon_err, hr_chan_names{kE}, ...
+        sprintf('%s (median)', this_info.type), 'recon_from_L2_dist');
+    savefig(fh, fullfile(sr_dirs.results, 'res_figs', sprintf('med_l2_recon_err_%s.fig', this_info.type)));
+    
+    % violin plots
+    % Score dist, ratio of real to null model
+    dist_ratio = all_dists{kE} ./ all_dists_null{kE};
+    [fh, pval] = make_violin_from_mats(dist_ratio, chan_names{kE}, 'right');
+    ylabel('Score distance (frac. of null model)');
+    title({'Euclidean distance of aligned NMF scores, compared to null model', ...
+        this_info.type}, 'Interpreter', 'none');
+    savefig(fh, fullfile(sr_dirs.results, 'res_figs', ...
+        sprintf('dist_vs_null_violin_%s.fig', this_info.type)));
+    exp_info(kE).dist_vs_null_pval = pval;
+    fprintf('Score distance vs. null, %s, cross- vs. same-region:\n p = %g\n', this_info.type, pval);
+    
+    % Reconstruction error
+    [fh, pval] = make_violin_from_mats(all_recon_err{kE}, chan_names{kE}, 'right');
+    ylabel('Reconstruction error');
+    title({'Cross-channel reconstruction error from aligned NMF scores', ...
+        this_info.type}, 'Interpreter', 'none');
+    savefig(fh, fullfile(sr_dirs.results, 'res_figs', ...
+        sprintf('recon_err_violin_%s.fig', this_info.type)));
+    exp_info(kE).recon_err_pval = pval;
+    fprintf('Reconstruction error, %s, cross- vs. same-region:\n p = %g\n', this_info.type, pval);
 end
+
+%% Finally, save
+save(fullfile(sr_dirs.results, 'analysis_info.mat'), 'exp_info');
+
+%% Function to make violin plots
+
+function [hf, p] = make_violin_from_mats(mats_over_days, chan_names, ttest_tail, b_include_self)
+% make violin plot to compare a quantity for cross-region pairs, witin-region pairs, and self-loops.
+% chan_names should be in "region_layer" format, not the "human-readable" format.
+% ttest_tail: 'right' means hypothesis is across > within for this measure and vice versa.
+
+if ~exist('b_include_self', 'var')
+    b_include_self = true;
+end
+
+types = {'Single channel replication', 'Within region', 'Across regions'};
+
+if ~b_include_self
+    types = types(2:end);
+end
+
+type_snames = cellfun(@matlab.lang.makeValidName, types, 'uni', false);
+n_types = length(types);
+
+n_chans = length(chan_names);
+this_regions = strtok(chan_names, '_');
+same_region = this_regions == this_regions';
+
+% get indices of each type of pair
+self_loops = find(eye(n_chans));
+linear_inds = {
+	setdiff(find(same_region), self_loops)
+    find(~same_region)
+    };
+
+if b_include_self
+    linear_inds = [{self_loops}; linear_inds];
+end
+
+vals_bytype = struct;
+
+for kT = 1:n_types
+    tname = type_snames{kT};
+    
+    vals_perday = cellfun(@(mat) mat(linear_inds{kT}), num2cell(mats_over_days, [1, 2]), 'uni', false);
+    vals_bytype.(tname) = vertcat(vals_perday{:});
+end
+
+hf = figure;
+violinplot(vals_bytype);
+xticklabels(types);
+
+[~, p] = ttest2(vals_bytype.AcrossRegions, vals_bytype.WithinRegion, 0.01, ttest_tail);
+end
+
+%% old code for separate real and null dist violin plots
+%     hf = figure; hold on;
+%     viol1 = violinplot(dists_bytype, [], 'ViolinColor', [1, 0, 0]);
+%     viol2 = violinplot(dists_null_bytype, [], 'ViolinColor', [0.5, 0.5, 0.5]);
+%     legend([viol1(1).ViolinPlot, viol2(1).ViolinPlot], 'Real data', 'Markov-chain generated', 'Location', 'southeast');
+%     xticklabels(types);
+%     ylabel('Euclidean score distance');
+%
+%     title({'Euclidean distance of aligned NMF scores between pairs of channels', ...
+%         exp_info(kE).type}, 'Interpreter', 'none');
+%
+%     savefig(hf, fullfile(sr_dirs.results, 'res_figs', ...
+%         sprintf('score_dist_violin_%s.fig', exp_info(kE).type)));
 
 %% make a graph plot out of it
 % med_kl_div_from_null_nonnan = med_dists_from_null;
@@ -261,65 +373,3 @@ end
 % colorbar;
 % savefig(hf_graph, fullfile(sr_dirs.results, 'res_figs', 'med_kl_div_fromnull_graphplot.fig'));
 
-%% Make violin plot
-
-types = {'Single channel replication', 'Within region', 'Across regions'};
-type_snames = cellfun(@matlab.lang.makeValidName, types, 'uni', false);
-n_types = length(types);
-
-for kE = 1:length(exp_info)
-    this_chan_names = chan_names{kE};
-    n_chans = length(this_chan_names);
-    this_regions = strtok(this_chan_names, '_');
-    same_region = this_regions == this_regions';
-    
-    % get indices of each type of pair
-    linear_inds = cell(n_types, 1);
-    linear_inds{1} = find(eye(n_chans));
-    linear_inds{2} = setdiff(find(same_region), linear_inds{1});
-    linear_inds{3} = find(~same_region);
-
-    dists_bytype = struct;
-    dists_null_bytype = struct;
-    dists_frac_of_null = struct;
-    
-    for kT = 1:n_types
-        %mean_kldiv_oftype = @(mat) mean(mat(linear_inds{kT}));
-        tname = type_snames{kT};
-
-        dists_perday = cellfun(@(mat) mat(linear_inds{kT}), num2cell(all_dists{kE}, [1, 2]), 'uni', false);
-        dists_bytype.(tname) = vertcat(dists_perday{:});
-
-        dists_null_perday = cellfun(@(mat) mat(linear_inds{kT}), num2cell(all_dists_null{kE}, [1, 2]), 'uni', false);
-        dists_null_bytype.(tname) = vertcat(dists_null_perday{:});
-
-        dists_frac_of_null.(tname) = dists_bytype.(tname) ./ dists_null_bytype.(tname);
-    end
-
-%     hf = figure; hold on;
-%     viol1 = violinplot(dists_bytype, [], 'ViolinColor', [1, 0, 0]);
-%     viol2 = violinplot(dists_null_bytype, [], 'ViolinColor', [0.5, 0.5, 0.5]);
-%     legend([viol1(1).ViolinPlot, viol2(1).ViolinPlot], 'Real data', 'Markov-chain generated', 'Location', 'southeast');
-%     xticklabels(types);
-%     ylabel('Euclidean score distance');
-%
-%     title({'Euclidean distance of aligned NMF scores between pairs of channels', ...
-%         exp_info(kE).type}, 'Interpreter', 'none');
-%
-%     savefig(hf, fullfile(sr_dirs.results, 'res_figs', ...
-%         sprintf('score_dist_violin_%s.fig', exp_info(kE).type)));
-
-    hf = figure;
-    viol = violinplot(dists_frac_of_null);
-    xticklabels(types);
-    ylabel('Score distance (frac. of null model)');
-    title({'Euclidean distance of aligned NMF scores, compared to null model', ...
-        exp_info(kE).type}, 'Interpreter', 'none');
-    savefig(hf, fullfile(sr_dirs.results, 'res_figs', ...
-        sprintf('dist_vs_null_violin_%s.fig', exp_info(kE).type)));
-
-    [h, p] = ttest2(dists_frac_of_null.(type_snames{3}), dists_frac_of_null.(type_snames{2}), ...
-        0.01, 'right');
-
-    fprintf('%s: p = %g\n', exp_info(kE).type, p);
-end
